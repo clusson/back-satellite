04c3e6efe12993700b6cbbb7cba82720bc48ba0b
Clément Lusson <clementlusson@gmail.com>
2018-01-22 19:38:35 +0100

    updates

--- dist/server.js
+++ dist/server.js
@@ -27,30 +27,22 @@ app.get('/keys', async (req, res, next) => {
 
 app.get('/key/:id', async (req, res, next) => {
     try {
-        const currKey = await db.get('SELECT' + req.params.id + ' FROM keys ORDER BY date DESC')
+        const currKey = await db.get('SELECT' + req.params.id + ' FROM keys ORDER BY date DESC');
         res.send(currKey);
     } catch (err) {
-        next(err)
+        next(err);
     }
 });
 
 app.put('/key', async (req, res, next) => {
-    const lastKey = req.body.id
-    Promise.resolve()
-        .then(() =>
-            db.run("INSERT INTO keys VALUES (" + lastKey + ")"),
-        res.send(lastKey)
-        )
-        .catch((err) =>
-            next(err)
-        )
+    const lastKey = req.body.id;
+    Promise.resolve().then(() => db.run("INSERT INTO keys VALUES (" + lastKey + ")"), res.send(lastKey)).catch(err => next(err));
 });
 
 Promise.resolve()
-    // First, try to open the database
-    .then(() => db.open('./database/satellite', { Promise }))
-    // Display error message if something went wrong
-    .catch((err) => console.error(err.stack))
-    // Finally, launch the Node.js app
-    .finally(() => app.listen(port),
-    console.log('Connected on port ' + port))
+// First, try to open the database
+.then(() => db.open('./database/satellite', { Promise }))
+// Display error message if something went wrong
+.catch(err => console.error(err.stack))
+// Finally, launch the Node.js app
+.finally(() => app.listen(port), console.log('Connected on port ' + port));
