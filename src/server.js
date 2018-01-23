515d3693a693111369b76aa8b59e2f94cd1d38f5
nathan <grimaudathan@gmail.com>
2018-01-23 15:59:56 +0100

    fix(some things)

--- src/server.js
+++ src/server.js
@@ -1,93 +1,73 @@
 const express = require('express');
 const db = require('sqlite');
-const Promise = require('bluebird');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 const { resolve } = require('url');
-
+const fetch = require('node-fetch')
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
-const amiralPort = 4200;
-const currKey = db.get('SELECT MAX(date) FROM keys');
+const port = process.env["PORT"] || 3002;
+const amiralPort =  process.env["AMIRAL_PORT"] || 3000;
+const name = process.env["NAME"] || "charles_de_gaulle"
+const firstKey = process.env["KEY"] || ""
+const fiveMinutes =  5 * 60 * 1000
+function routes(app,db){
 
-app.listen(amiralPort, async (req, res, next) => {
-    const rule = new schedule.RecurrenceRule();
-    rule.minute = new schedule.Range(0, 59, 5);
-
-    schedule.scheduleJob(rule, function () {
-        // call the amiral back every 5 min with lastKey
-        app.put('/key', async (req, res, next) => {
-            currKey = req.body.id
-            const lastKey = await res.body.id
-            Promise.resolve()
-                .then(() =>
-                    db.run("Insert into keys (id) values (?)", lastKey)
-                )
-                .catch((err) =>
-                    next(err)
-                )
-        });
-    })
-
-});
-
-
-app.get('/', async (req, res, next) => {
-    Promise.resolve()
-        .then(() =>
-            res.json({ message: 'hooray! welcome to our api!' + moment().format() })
-        )
-        .catch((err) =>
-            next(err)
-        )
-});
-
-// Get the new key from amiral when satellite instance
-app.get('/key', async (req, res, next) => {
-    const newKey = await res.params.id
-    Promise.resolve()
-        .then(() =>
-            db.run("Insert into keys (id) values (?)", newKey)
-        )
-        .catch((err) =>
-            next(err)
-        )
-});
-
-app.get('/keys', async (req, res, next) => {
-    const keys = await db.all('SELECT * FROM keys'); // <=
-    Promise.resolve()
-        .then(() =>
-            res.send(keys)
-        )
-        .catch((err) =>
-            next(err)
-        )
-});
+    let lastKey = ""
+    function getKey(key){
+        fetch(`http://localhost:${amiralPort}/code`,{
+                method:"put",
+                body: JSON.stringify({
+                    key: key,
+                    name: name
+                })
+        })
+        .then(response => response.json())
+        .then((response)=>{
+            if(lastKey !== response.id){
+                console.log('got new key', response.id)
+                lastKey = response.id
+                return db.run("insert into keys (id) values (?)", response.id)
+            }
+            else{
+                console.log("key already exists, waiting for next one")
+                return Promise.resolve(response.id)
+            }
+        }).catch(err => console.error(err));
+    }
 
 
-app.get('/key/:id', async (req, res, next) => {
-    const currKey = await db.get('SELECT * FROM keys WHERE id =' + req.params.id)
-    Promise.resolve()
-        .then(() =>
-            res.send(currKey)
-        )
-        .catch((err) =>
-            next(err)
-        )
-});
+   
+    // Get the new key from amiral when satellite instance
+    app.get('/key', async (req,res) => {
+        const results = await db.get('select id from keys where date = (select MAX(date) from keys)')
+        res.send(results)
+    });
+    app.get('/keys', async (req, res, next) => {
+        const keys = await db.all('SELECT * FROM keys'); // <=
+        res.send(keys)
+    });
+    
+    if (firstKey!==""){
+        getKey(firstKey)
+    } else {
+       db.get('select id from keys where date = (select MAX(date) from keys)')
+       .then(key => getKey(key.id))
+    }
+    setInterval(()=>{
+        db.get('select id from keys where date = (select MAX(date) from keys)')
+            .then(key => getKey(key.id))
+    },fiveMinutes)
+}
 
 
 
-Promise.resolve()
-    // First, try to open the database
-    .then(() => db.open('./satellite.db', { Promise }))
-    // Display error message if something went wrong
+db.open('./satellite.db')
+    .then(db => routes(app, db))
+    .then(() => app.listen(port))
+    .then(()=>console.log('Connected on port ' + port))
     .catch((err) => console.error(err.stack))
-    // Finally, launch the Node.js app
-    .finally(() => app.listen(port),
-    console.log('Connected on port ' + port))
+
